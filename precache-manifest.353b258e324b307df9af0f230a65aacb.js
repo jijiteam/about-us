bc2925675d2a55b5cd55c3da41b3c8f001617c50
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2019-06-29 22:48:02 +0200

    Updates

---
+++ precache-manifest.353b258e324b307df9af0f230a65aacb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "45688a51559ef234703d753390d1cdb5",
+    "url": "/about-us/static/media/rds.45688a51.png"
+  },
+  {
+    "revision": "aea3b6f2d52588be288a",
+    "url": "/about-us/static/css/main.016a897a.chunk.css"
+  },
+  {
+    "revision": "017afdc79d40983d0140b417a928187b",
+    "url": "/about-us/static/media/preview.017afdc7.jpg"
+  },
+  {
+    "revision": "18b38b8b8c63d5eccf14",
+    "url": "/about-us/static/js/1.18b38b8b.chunk.js"
+  },
+  {
+    "revision": "6478fd92eb82d46b27b0",
+    "url": "/about-us/static/js/runtime~main.6478fd92.js"
+  },
+  {
+    "revision": "a5a3efc32135a6b4f1c3a1629af55b0b",
+    "url": "/about-us/static/media/img-placeholder-dark.a5a3efc3.jpg"
+  },
+  {
+    "revision": "aea3b6f2d52588be288a",
+    "url": "/about-us/static/js/main.aea3b6f2.chunk.js"
+  },
+  {
+    "revision": "b1156355de9691d768df19a8a2b44da4",
+    "url": "/about-us/static/media/fr.b1156355.svg"
+  },
+  {
+    "revision": "4f1b7af5c0bae6aae85c3e7ba9401a85",
+    "url": "/about-us/static/media/gb.4f1b7af5.svg"
+  },
+  {
+    "revision": "f8952213641bba462c7314007909d394",
+    "url": "/about-us/static/media/fr.f8952213.svg"
+  },
+  {
+    "revision": "50f7fd14ca63b8b05bdbf0554a5092d8",
+    "url": "/about-us/static/media/gb.50f7fd14.svg"
+  },
+  {
+    "revision": "18b38b8b8c63d5eccf14",
+    "url": "/about-us/static/css/1.f7d42505.chunk.css"
+  },
+  {
+    "revision": "63174b5842b76b0888b13821cd751dcf",
+    "url": "/about-us/index.html"
+  }
+];
